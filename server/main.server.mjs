ce093fb41e643544564b9499316c432b55740c42
ftfernandes <ftfernandes@gmail.com>
2025-05-12 16:16:55 -0300

    Auto-generated commit

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-DW7A24LP.mjs";import"./chunk-RMPKOU42.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-2O6G2LOL.mjs";import"./chunk-RMPKOU42.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
